ab6c206b3490066304472ca334eba90a9d99f8a9
amie-affection <amie.affection@gmail.com>
2020-05-27 23:49:36 +0300

    modified files

---
+++ src/js/apiService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// 16769549-77e013c4c462af3357f93945e
+
+// {
+//   "comments": 78,
+//   "downloads": 63296,
+//   "largeImageURL": "https://pixabay.com/get/57e5d54b4c53af14f6da8c7dda793376173cd8e7524c704c702873dc9f44c551_1280.jpg",
+//   "likes": 575,
+//   "views": 127450,
+//   "webformatURL": "https://pixabay.com/get/57e5d54b4c53af14f6da8c7dda793376173cd8e7524c704c702873dc9f44c551_640.jpg",
+// }
+
+const moreButton = document.querySelector('.buttonMore');
+
+moreButton.addEventListener('click', showMore);
+
+const showMore = () {
+
+}
