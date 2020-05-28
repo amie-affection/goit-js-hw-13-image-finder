5cf044037226063a26c7990689d6b0fa755635c1
amie-affection <amie.affection@gmail.com>
2020-05-28 22:18:20 +0300

    modified files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './styles.css';
+import api from './js/apiService';
+import card from './card.hbs';
+
+const form = document.forms.namedItem('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.buttonMore');
+
+loadMoreBtn.addEventListener('click', () => {
+  api.getImage().then(res => createCardItem(res));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // console.log(form.elements.query.value);
+  const value = form.elements.query.value;
+    api.query = value;
+    gallery.innerHTML = '';
+    api.getImage().then(res => createCardItem(res));
+    form.reset();
+});
+// console.log(form);
+
+const createCardItem = array => {
+  const cardList = array.reduce((acc, item) => {
+    return (acc += card(item));
+  }, '');
+  gallery.insertAdjacentHTML('beforeend', cardList);
+};
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,20 +1,17 @@
 import axios from 'axios';
 
-// 16769549-77e013c4c462af3357f93945e
+const api = {
+  apiKey: '16769549-77e013c4c462af3357f93945e',
+  searchQuery: '',
+  page: 1,
 
-// {
-//   "comments": 78,
-//   "downloads": 63296,
-//   "largeImageURL": "https://pixabay.com/get/57e5d54b4c53af14f6da8c7dda793376173cd8e7524c704c702873dc9f44c551_1280.jpg",
-//   "likes": 575,
-//   "views": 127450,
-//   "webformatURL": "https://pixabay.com/get/57e5d54b4c53af14f6da8c7dda793376173cd8e7524c704c702873dc9f44c551_640.jpg",
-// }
+  async getImage() {
+    const res = await axios.get(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${this.apiKey}`,
+    );
+    this.page += 1;
+    return res.data.hits;
+  },
+};
 
-const moreButton = document.querySelector('.buttonMore');
-
-moreButton.addEventListener('click', showMore);
-
-const showMore = () {
-
-}
+export default api;
